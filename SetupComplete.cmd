5929395dca6758529c2d13701c149e7344478ed6
Darius Bakunas-Milanowski <bakunas@gmail.com>
2014-12-30 21:51:58 -0500

    Minor fix

--- SetupComplete.cmd
+++ SetupComplete.cmd
@@ -1,3 +1,15 @@
 del /q /f c:\windows\system32\sysprep\unattend.xml
-
 del /q /f c:\windows\panther\unattend.xml
+
+pushd c:\Windows\Setup\Scripts
+
+echo %PROCESSOR_ARCHITECTURE% | find /i "x86" > nul
+if %errorlevel%==0 (
+    :: 32bit Windows
+    call puppet32.bat
+) else (
+    :: 64bit Windows
+    call puppet64.bat
+)
+
+popd
