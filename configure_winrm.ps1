ef2ab097c284ec77eb0081a29105ee7399ee8b56
Darius Bakunas-Milanowski <bakunas@gmail.com>
2015-01-02 20:40:02 -0500

    Final touches

--- configure_winrm.ps1
+++ configure_winrm.ps1
@@ -1,16 +1,8 @@
 Enable-PSRemoting -SkipNetworkProfileCheck -Force
 
-# winrm quickconfig -q
-# winrm quickconfig -transport:http
 winrm set winrm/config '@{MaxTimeoutms="1800000"}'
 winrm set winrm/config/winrs '@{MaxMemoryPerShellMB="512"}'
 winrm set winrm/config/service '@{AllowUnencrypted="true"}'
 winrm set winrm/config/service/auth '@{Basic="true"}'
 winrm set winrm/config/client/auth '@{Basic="true"}'
-set-service winrm -startuptype automatic
-# netsh advfirewall firewall set rule group="Windows Remote Management" new enable=yes
-#net stop winrm
-#sc config winrm start= auto
-#net start winrm
-
-Stop-Transcript
+set-service winrm -StartupType Auto
